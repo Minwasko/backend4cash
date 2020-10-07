30f68dcd3f53ffec4cb84ca8afcd3a5fd506d18c
Gleb Komissarov <glkomi@ttu.ee>
2020-10-07 18:03:16 +0300

    tryna connect to db in tests to localize the issue

--- dbstart.sh
+++ dbstart.sh
@@ -1,3 +1,3 @@
 docker pull mysql:latest
 #TODO: maybe make separate db for prod/dev
-docker run --name mysql --net=network4cash --net-alias=mysql -e MYSQL_ROOT_PASSWORD=vovan -d mysql:latest
+docker run --name mysql -p 3306:3306 --net=network4cash --net-alias=mysql -e MYSQL_ROOT_PASSWORD=vovan -d mysql:latest
