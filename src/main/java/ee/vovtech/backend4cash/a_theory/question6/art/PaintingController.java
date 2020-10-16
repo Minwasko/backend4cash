3e27a059eebc6016aa0c7432a4bf9dd8e5ef31c4
Gleb Komissarov <glkomi@ttu.ee>
2020-10-16 17:50:18 +0300

    Question 6 Art - Gleb Komissarov Added missing files

--- src/main/java/ee/vovtech/backend4cash/a_theory/question6/art/PaintingController.java
+++ src/main/java/ee/vovtech/backend4cash/a_theory/question6/art/PaintingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@RequestMapping("/paintings")
 public class PaintingController {
 
     private ArtCollector artCollector = new ArtCollector();
