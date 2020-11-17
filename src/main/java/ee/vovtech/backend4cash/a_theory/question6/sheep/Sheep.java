3ea77090782b97e34a1e4a507ed550facefcb045
Gleb Komissarov <glkomi@ttu.ee>
2020-11-17 15:17:40 +0200

    tryna trigger paiplain to test stuff

--- src/main/java/ee/vovtech/backend4cash/a_theory/question6/sheep/Sheep.java
+++
@@ -1,9 +0,0 @@
-package ee.vovtech.backend4cash.a_theory.question6.sheep;
-
-public class Sheep {
-
-    private Long id;
-    private String name;
-    private Integer age;
-    private String color;
-}
