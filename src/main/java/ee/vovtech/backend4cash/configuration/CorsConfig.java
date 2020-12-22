1c88b98171f6ddf7109706672217ee44f01e892c
Gleb Komissarov <glkomi@ttu.ee>
2020-12-22 22:11:39 +0200

    trying to implement features shit

---
+++ src/main/java/ee/vovtech/backend4cash/configuration/CorsConfig.java
@@ -0,0 +1,15 @@
+package ee.vovtech.backend4cash.configuration;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
