5f5fab67ca5524ad3c60f1085e826df267abd283
Gleb Komissarov <glkomi@ttu.ee>
2020-12-27 01:56:19 +0200

    Trying to add some scheduled thingies

--- src/main/java/ee/vovtech/backend4cash/service/currency/UpdatePricesService.java
+++ src/main/java/ee/vovtech/backend4cash/service/currency/UpdatePricesService.java
@@ -1,23 +1,32 @@
 package ee.vovtech.backend4cash.service.currency;
 
-import ee.vovtech.backend4cash.repository.CurrencyRepository;
+import ee.vovtech.backend4cash.service.currency.CurrencyPriceService.UpdateTime;
+import ee.vovtech.backend4cash.model.Currency;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
-@Slf4j
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 @Service
 @AllArgsConstructor
 public class UpdatePricesService {
 
-    private final CurrencyRepository currencyRepository;
+    private final CurrencyService currencyService;
+    private final CurrencyPriceService currencyPriceService;
     private final UpdatePricesConfig updatePricesConfig;
 
     @Scheduled(cron = "${coins.cron}", zone = "Europe/Tallinn")
-    public void updatePrices() {
+    public void updatePrices() throws UnirestException {
         if (updatePricesConfig.notUpdating()) {
             System.out.println("not doing it");
+        }
+        List<Currency> currencies = currencyService.findAll();
+        for(Currency currency : currencies){
+            currencyPriceService.updatePrice(currency.getName(), UpdateTime.HOUR);
+            System.out.println("Updated " + currency.getName());
         }
             System.out.println("doing it");
         // TODO
