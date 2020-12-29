cfee98c3ce5cbe78d6cf1807730834496963599b
Gleb Komissarov <glkomi@ttu.ee>
2020-12-30 01:44:46 +0200

    removed redundant method

--- src/main/java/ee/vovtech/backend4cash/service/user/UserService.java
+++ src/main/java/ee/vovtech/backend4cash/service/user/UserService.java
@@ -79,9 +79,4 @@ public class UserService {
         }
     }
 
-    // !!! temporary solution for id thingies in users. Has to be fixed l8r
-    public boolean idIsTaken(long userID){
-        return userRepository.existsById(userID);
-    }
-
 }
